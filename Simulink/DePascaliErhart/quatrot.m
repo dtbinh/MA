5401a015f38c5954eb894dd41c55937b56eb535c
mangerer <martin.angerer@tum.de>
2015-11-02 19:23:59 +0100

    stable desired internal force

--- Simulink/DePascaliErhart/quatrot.m
+++ Simulink/DePascaliErhart/quatrot.m
@@ -1,7 +1,7 @@
 function [rw] = quatrot(q,r)
 % Quaternion q based rotation of vector r, e.g. from object-based to world
 % frame
-rwq = quatmultiply(quatconj(q'),quatmultiply([0 r'],q'))';
+rwq = quatmultiply((q'),quatmultiply([0 r'],quatconj(q')))';
 rw = rwq(2:4);
 end
 
